e3695ee9382bd8906b7f0265001500cf67d4fcd8
Marconi Gomes <marconi.gomes7@gmail.com>
2020-02-22 14:45:48 -0300

    First files first.

---
+++ checkers/data_checker.py
@@ -0,0 +1,26 @@
+import glob
+import logging
+
+class DataChecker:
+    """This class represents the checker for the Graphs' data."""
+
+    __author__ = "Marconi Gomes"
+
+    @staticmethod
+    def check_files():
+        """Checks the test files using glob."""
+        print("Checking files...")
+
+        try:
+            filenames = glob.glob("./test_data/*.txt")
+            filenames.sort()
+            for i in range(1, 16):
+                filename = "./test_data/test" + str(i) + ".txt"
+                if not (filename in filenames):
+                    raise Exception(filename + " not found.")
+
+            print("OK.")
+
+        except Exception as ex:
+            logging.exception(str(ex))
+            raise Exception
---
+++ main.py
@@ -0,0 +1,5 @@
+from checkers.data_checker import DataChecker
+
+if __name__ == "__main__":
+    DataChecker.check_files()
+    print("Howdy.")
